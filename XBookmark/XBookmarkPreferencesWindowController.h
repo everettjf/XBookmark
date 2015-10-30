852e4ce60ee2f2ffd05ba73d0c81a200f36a8f29
everettjf <everettjf@live.com>
2015-10-30 23:15:28 +0800

    preference

---
+++ XBookmark/XBookmarkPreferencesWindowController.h
@@ -0,0 +1,13 @@
+//
+//  XBookmarkPreferencesWindowController.h
+//  XBookmark
+//
+//  Created by everettjf on 10/30/15.
+//  Copyright © 2015 everettjf. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface XBookmarkPreferencesWindowController : NSWindowController
+
+@end
---
+++ XBookmark/XBookmarkPreferencesWindowController.m
@@ -0,0 +1,31 @@
+//
+//  XBookmarkPreferencesWindowController.m
+//  XBookmark
+//
+//  Created by everettjf on 10/30/15.
+//  Copyright © 2015 everettjf. All rights reserved.
+//
+
+#import "XBookmarkPreferencesWindowController.h"
+
+@interface XBookmarkPreferencesWindowController ()
+
+@end
+
+@implementation XBookmarkPreferencesWindowController
+
+-(instancetype)init{
+    return [self initWithWindowNibName:@"XBookmarkPreferencesWindowController"];
+}
+
+- (void)windowDidLoad {
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+- (IBAction)restoreToDefaultShortcut:(id)sender {
+}
+- (IBAction)okClicked:(id)sender {
+}
+
+@end
